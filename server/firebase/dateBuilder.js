bc2655c88bd636562433f6ecf296edd2a914f1de
NikolasMelui <slak@samaradom.ru>
2018-02-26 21:36:53 +0400

    Add pallet for dateBuilder middleware function.

--- server/firebase/dateBuilder.js
+++ server/firebase/dateBuilder.js
@@ -1,3 +1,23 @@
+const dateStringify = require('./dateStringify');
+
 module.exports = async (ctx, next) => {
+	let procurementPlanDate,
+		shedulePlanDate,
+		noticeDate,
+		endAddOfApplicationDate,
+		firstPartsConsiderationDate,
+		auctionDate,
+		finalProtocolDate,
+		contractDate;
+
+	ctx.request.body.procurementPlanDate = procurementPlanDate;
+	ctx.request.body.shedulePlanDate = shedulePlanDate;
+	ctx.request.body.noticeDate = noticeDate;
+	ctx.request.body.endAddOfApplicationDate = endAddOfApplicationDate;
+	ctx.request.body.firstPartsConsiderationDate = firstPartsConsiderationDate;
+	ctx.request.body.auctionDate = auctionDate;
+	ctx.request.body.finalProtocolDate = finalProtocolDate;
+	ctx.request.body.contractDate = contractDate;
+
 	await next();
 };
